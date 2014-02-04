6643e0b2defbcdde8f87289a732dcfe779873196
NicoVermeir <nico_vermeir@hotmail.com>
2014-02-04 15:38:53 +0100

    created new VSpackage project to push to the gallery

---
+++ XamlStyler.Package/PackageOptions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.Shell;
+using XamlStyler.Core.Options;
+
+namespace NicoVermeir.XamlStyler_Package
+{
+    public class PackageOptions : DialogPage
+    {
+        private readonly IStylerOptions _options;
+
+        public PackageOptions()
+        {
+            _options = new StylerOptions();
+        }
+
+        public override object AutomationObject
+        {
+            get { return _options; }
+        }
+    }
+}
---
+++ XamlStyler.Package/PkgCmdID.cs
@@ -0,0 +1,13 @@
+﻿// PkgCmdID.cs
+// MUST match PkgCmdID.h
+using System;
+
+namespace NicoVermeir.XamlStyler_Package
+{
+    static class PkgCmdIDList
+    {
+        public const uint cmdidBeautifyXaml =        0x100;
+
+
+    };
+}
