a88e1dd2b5fcfec5f54bc8a5ca7e8826741296f2
PolarbearDK <philip@dsdevelopment.dk>
2015-07-22 12:30:57 +0200

    Reorder setting properties into different categories

--- XamlStyler.Service/Options/StylerOptions.cs
+++ XamlStyler.Service/Options/StylerOptions.cs
@@ -149,8 +149,16 @@ namespace XamlStyler.Core.Options
         [DefaultValue("xmlns, xmlns:x")]
         public string AttributeOrderWpfNamespace { get; set; }
 
+        [Category("Attribute Ordering Rule Groups")]
+        [DisplayName("Put attributes order rule groups on separate lines")]
+        [Description(
+    "Put attributes belonging to different order rules on different lines (keep identical order rules on same line if possible)"
+    )]
+        [DefaultValue(false)]
+        public bool PutAttributeOrderRuleGroupsOnSeparateLines { get; set; }
 
         //Markup Extension
+
         [Category("Markup Extension")]
         [DisplayName("Enable Markup Extension Formatting")]
         [Description(
@@ -159,9 +167,7 @@ namespace XamlStyler.Core.Options
         [DefaultValue(true)]
         public bool FormatMarkupExtension { get; set; }
 
-
-        // New Line
-        [Category("New Line")]
+        [Category("Markup Extension")]
         [DisplayName("Keep x:Bind statements on same line")]
         [Description(
             "Defines whether the attribute(s) of an x:Bind statement will be kept on the same line \r\n\r\nDefault Value: true"
@@ -169,7 +175,7 @@ namespace XamlStyler.Core.Options
         [DefaultValue(true)]
         public bool KeepxBindOnSameLine { get; set; }
 
-        [Category("New Line")]
+        [Category("Markup Extension")]
         [DisplayName("Keep Binding statements on same line")]
         [Description(
            "Defines whether the attribute(s) of a Binding statement will be kept on the same line \r\n\r\nDefault Value: false"
@@ -177,7 +183,8 @@ namespace XamlStyler.Core.Options
         [DefaultValue(false)]
         public bool KeepBindingsOnSameLine { get; set; }
 
-        [Category("New Line")]
+        // Attribute formatting
+        [Category("Attribute formatting")]
         [DisplayName("Attribute tolerance")]
         [Description(
             "Defines the attribute number tolerance before XamlStyler starts to break attributes into new lines. A value less than 1 meaning no tolerance.\r\ne.g., when this setting is 2\r\n\r\nBEFORE BEAUTIFY:\r\n<TextBlock x:Name=\"m_sample\"\r\n    Text=\"asdf\" />\r\n\r\nAFTER BEAUTIFY:\r\n<TextBlock x:Name=\"m_sample\" Text=\"asdf\" />\r\nDefault Value: 2"
@@ -185,7 +192,7 @@ namespace XamlStyler.Core.Options
         [DefaultValue(2)]
         public int AttributesTolerance { get; set; }
 
-        [Category("New Line")]
+        [Category("Attribute formatting")]
         [DisplayName("Position first attribute on same line as start tag")]
         [Description(
             "Defines whether the first line of attribute(s) shall appear on the same line as the element's start tag.\r\ne.g., when this setting is true\r\n\r\nBEFORE BEAUTIFY:\r\n\"<element a='xyz' b='xyz'>  </element>\"\r\n\r\nAFTER BEAUTIFY:\r\n\"<element a='xyz'\r\n        b='xyz'>\r\n</element>\"\r\n\r\nDefault Value: true"
@@ -193,7 +200,7 @@ namespace XamlStyler.Core.Options
         [DefaultValue(true)]
         public bool KeepFirstAttributeOnSameLine { get; set; }
 
-        [Category("New Line")]
+        [Category("Attribute formatting")]
         [DisplayName("Max attribute characters per line")]
         [Description(
             "Defines the maximum charater length (not including indentation characters) of attributes an element can have on each line after the start tag. A value less than 1 meaning no limit.\r\nDefault Value: 0"
@@ -201,15 +208,15 @@ namespace XamlStyler.Core.Options
         [DefaultValue(0)]
         public int MaxAttributeCharatersPerLine { get; set; }
 
-        [Category("New Line")]
-        [DisplayName("Max attribute number per line")]
+        [Category("Attribute formatting")]
+        [DisplayName("Max attributes per line")]
         [Description(
             "Defines the maximum number of attributes an element can have on each line after the start tag. A value less than 1 meaning no limit.\r\nDefault Value: 1"
             )]
         [DefaultValue(1)]
         public int MaxAttributesPerLine { get; set; }
 
-        [Category("New Line")]
+        [Category("Attribute formatting")]
         [DisplayName("Elements no line break between attributes")]
         [Description(
             "Defines a list of elements whose attributes shall not be broken into lines.\r\nDefault Value: RadialGradientBrush, GradientStop, LinearGradientBrush, ScaleTransfom, SkewTransform, RotateTransform, TranslateTransform, Trigger, Setter"
@@ -219,21 +226,14 @@ namespace XamlStyler.Core.Options
             )]
         public string NoNewLineElements { get; set; }
 
-        [Category("New Line")]
-        [DisplayName("Put attributes order rule groups on separate lines")]
-        [Description(
-            "Put attributes belonging to different order rules on different lines (keep identical order rules on same line if possible)"
-            )]
-        [DefaultValue(false)]
-        public bool PutAttributeOrderRuleGroupsOnSeparateLines { get; set; }
-
-        [Category("New Line")]
+        // Element formatting
+        [Category("Element formatting")]
         [DisplayName("Put ending bracket on new line")]
         [Description("Defines whether to put \">\" or \"/>\" on a new line.\r\nDefault Value: false")]
         [DefaultValue(false)]
         public bool PutEndingBracketOnNewLine { get; set; }
 
-        [Category("New Line")]
+        [Category("Element formatting")]
         [DisplayName("Remove ending tag of empty element")]
         [Description(
             "Defines whether to remove the ending tag of an empty element.\r\ne.g., when this setting is true\r\n\r\nBEFORE BEAUTIFY:\r\n\"<element>  </element>\"\r\n\r\nAFTER BEAUTIFY:\r\n\"<element />\"\r\n\r\nDefault Value: true"
@@ -241,7 +241,7 @@ namespace XamlStyler.Core.Options
         [DefaultValue(true)]
         public bool RemoveEndingTagOfEmptyElement { get; set; }
 
-        [Category("New Line")]
+        [Category("Element formatting")]
         [DisplayName("Space before closing slash in self closing element")]
         [Description(
             "Defines whether to have a space before slash in self closing elements \r\ne.g., when\r\ntrue <br />\r\nfalse <br/>\r\n\r\nDefault Value: true"
@@ -249,7 +249,7 @@ namespace XamlStyler.Core.Options
         [DefaultValue(true)]
         public bool SpaceBeforeClosingSlash { get; set; }
 
-        [Category("New Line")]
+        [Category("Element formatting")]
         [DisplayName("Root element line breaks between attributes")]
         [Description(
             "Defines if attributes of the document root element are broken into separate lines or not.\r\nDefault = use same rules as other elements"
@@ -264,21 +264,20 @@ namespace XamlStyler.Core.Options
         [DefaultValue(true)]
         public bool BeautifyOnSave { get; set; }
 
-        // Content Order
-
-        [Category("Content Order")]
+        // Element reordering
+        [Category("Element reordering")]
         [DisplayName("Reorder Grid panel children by row/column")]
         [Description("Defines whether to reorder the children of a Grid by row/column.  When this is true, children will be reordered in an ascending fashion by looking at their attached Grid properties: first by Grid.Row, then by Grid.Column.")]
         [DefaultValue(true)]
         public bool ReorderGridChildren { get; set; }
 
-        [Category("Content Order")]
+        [Category("Element reordering")]
         [DisplayName("Reorder Canvas panel children by left/top/right/bottom")]
         [Description("Defines whether to reorder the children of a Canvas by left/top/right/bottom.  When this is true, children will be reordered in an ascending fashion by looking at their attached Canvas properties: first by Canvas.Left, then by Canvas.Top, then by Canvas.Right, then by Canvas.Bottom.")]
         [DefaultValue(true)]
         public bool ReorderCanvasChildren { get; set; }
 
-        [Category("Content Order")]
+        [Category("Element reordering")]
         [DisplayName("Reorder Setters by")]
         [Description("Defines whether to reorder 'Setter' elements in style/trigger elements. When this is set, children will be reordered in an ascending fashion by looking at their Property and/or TargetName properties")]
         [DefaultValue(ReorderSettersBy.None)]
