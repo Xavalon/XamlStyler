17a4384d2a8405079e992afd557ae8c92a61c3fe
Dave Grochocki <dev@davernet.com>
2016-06-08 23:49:39 -0700

    Added option to remove design time references.

---
+++ XamlStyler.Core/DocumentManipulation/AttributeSelector.cs
@@ -0,0 +1,55 @@
+// © Xavalon. All rights reserved.
+
+using System;
+using System.ComponentModel;
+using System.Text.RegularExpressions;
+using System.Xml.Linq;
+
+namespace Xavalon.XamlStyler.Core.DocumentManipulation
+{
+    public class AttributeSelector : NameSelector
+    {
+        private Regex valueRegex;
+
+        private string value;
+        [DisplayName("Value")]
+        [Description("Match value by name. null/empty = all. 'DOS' Wildcards permitted.")]
+        public string Value
+        {
+            get { return this.value; }
+            set
+            {
+                this.value = value;
+                this.valueRegex = (this.value != null) ? new Wildcard(this.value) : null;
+            }
+        }
+
+        public AttributeSelector(string name, string value)
+            : base(name)
+        {
+            this.Value = value;
+        }
+
+        public AttributeSelector(string name, string @namespace, string value)
+            : base(name, @namespace)
+        {
+            this.Value = value;
+        }
+
+        public bool IsMatch(XAttribute attribute)
+        {
+            if (!this.valueRegex?.IsMatch(attribute.Value) ?? false)
+            {
+                return false;
+            }
+
+            return base.IsMatch(attribute.Name);
+        }
+
+        public override string ToString()
+        {
+            var prefix = ((this.Namespace != null) ? $"{this.Namespace}:" : String.Empty);
+            return $"{prefix}{this.Name}={this.Value}";
+        }
+    }
+}
