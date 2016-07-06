6aff74a69a502b54ba90424173683f1bcb588b3b
Ben Lacey <ben.lacey@audiogum.com>
2016-07-06 08:54:59 +0100

    Update version

--- XamlStyler.XamarinStudio/Properties/AddinInfo.cs
+++ XamlStyler.XamarinStudio/Properties/AddinInfo.cs
@@ -5,12 +5,12 @@ using Mono.Addins.Description;
 [assembly:Addin (
     "XamlStyler",
     Namespace = "Xavalon",
-    Version = "1.0"
+    Version = "1.0.1"
 )]
 
 [assembly:AddinName ("XamlStyler")]
 [assembly:AddinCategory ("IDE extensions")]
-[assembly:AddinDescription ("A Xamarin Studio port of the Visual Studio XamlStyler extension.")]
+[assembly:AddinDescription ("A Xamarin Studio port of the Visual Studio XamlStyler extension.  Reformat your XAML to be more readable, maintainable and beautiful!")]
 [assembly:AddinAuthor ("Xavalon & Ben Lacey")]
 
 [assembly:AddinDependency ("::MonoDevelop.Core", MonoDevelop.BuildInfo.Version)]
