1c8f6eab523759a729b96fd67369d13dd3528617
AndrewSt <andrew@andrewst.com>
2016-09-06 22:50:48 +0300

    Init new project  XamlStyler.CommandLine

---
+++ XamlStyler.CommandLine/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace XamlStyler.CommandLine
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                PrintHelp();
+                return 0;
+            }
+            return 0;
+        }
+        private static void PrintHelp()
+        {
+            var version = typeof(Program).Assembly.GetName().Version;
+
+            Console.WriteLine($"XamlStyler Version: {version}, Command line utility for styling xaml");
+            Console.WriteLine($"usage: XamlStyler <xaml-file> [-s <setings-file>]");
+        }
+    }
+}
