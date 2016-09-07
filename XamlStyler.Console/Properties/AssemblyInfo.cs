00d6f5ff3a8c76fca24b79dcf946e7c2ec1e4873
AndrewSt <andrew@andrewst.com>
2016-09-07 22:32:40 +0300

    add nuspec and batch file for paking nuget package

--- XamlStyler.Console/Properties/AssemblyInfo.cs
+++ XamlStyler.Console/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Xavalon")]
 [assembly: AssemblyProduct("XAML Styler")]
-[assembly: AssemblyCopyright("Copyright © 2016")]
+[assembly: AssemblyCopyright("Copyright © Xavalon 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
