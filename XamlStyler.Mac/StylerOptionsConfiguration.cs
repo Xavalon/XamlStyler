b111e10a2fd3a559d12c2057e7b0d24acd93f2c1
Taras Shevchuk <schew4uk@gmail.com>
2019-07-26 01:55:48 +0300

    External configuration support for Mac extension (#194)
    * Added external configurations support for Visual Studio for Mac
    
    * Updated Readme
    
    * Fixed crash on New Visual Studio for Mac
    
    * Update README.md

--- XamlStyler.Mac/StylerOptionsConfiguration.cs
+++ XamlStyler.Mac/StylerOptionsConfiguration.cs
@@ -1,13 +1,48 @@
-﻿using System;
-using System.IO;
-using MonoDevelop.Core;
+﻿using MonoDevelop.Core;
+using MonoDevelop.Ide.Gui;
+using MonoDevelop.Projects;
 using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using Xavalon.XamlStyler.Core.Options;
 
 namespace Xavalon.XamlStyler.Mac
 {
     public static class StylerOptionsConfiguration
     {
+        public static bool IsFormatableDocument(Document document)
+        {
+            if (document is null || document.IsViewOnly)
+            {
+                return false;
+            }
+
+            var isFormatableDocument = document.FileName.Extension.Equals(".xaml", StringComparison.OrdinalIgnoreCase);
+            return isFormatableDocument;
+        }
+
+        public static StylerOptions GetOptionsForDocument(string documentPath, Project project)
+        {
+            var stylerOptions = ReadFromUserProfile();
+            var configPath = GetConfigPathForDocument(documentPath, project);
+
+            if (configPath != null)
+            {
+                stylerOptions = (StylerOptions)stylerOptions.Clone();
+                stylerOptions.ConfigPath = configPath;
+            }
+
+            //TODO Add this feature support
+            if (stylerOptions.UseVisualStudioIndentSize)
+            {
+                stylerOptions.IndentSize = 4;
+            }
+
+            return stylerOptions;
+        }
+
         public static StylerOptions ReadFromUserProfile()
         {
             var filePath = GetOptionsFilePath().ToString();
@@ -29,8 +64,7 @@ namespace Xavalon.XamlStyler.Mac
             // Xamarin Forms defaults
             var options = new StylerOptions()
             {
-                IndentSize = 4,
-                KeepFirstAttributeOnSameLine = true
+                IndentSize = 4
             };
 
             try
@@ -68,6 +102,48 @@ namespace Xavalon.XamlStyler.Mac
         private static FilePath GetOptionsFilePath()
         {
             return UserProfile.Current.ConfigDir.Combine("xamlstyler.config");
+        }
+
+        private static string GetConfigPathForDocument(string documentPath, Project project)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(documentPath) || project is null)
+                {
+                    return null;
+                }
+
+                var projectFileName = project.FileName;
+                var projectRoot = string.IsNullOrEmpty(projectFileName) ? string.Empty : Path.GetDirectoryName(projectFileName);
+
+                var solution = project.ParentSolution;
+                var solutionFileName = solution.FileName;
+                var solutionRoot = string.IsNullOrEmpty(solutionFileName) ? string.Empty : Path.GetDirectoryName(solutionFileName);
+
+                var root = documentPath.StartsWith(solutionRoot, StringComparison.OrdinalIgnoreCase) ? solutionRoot : projectRoot;
+                var configPaths = GetConfigPathBetweenPaths(documentPath, root);
+
+                return configPaths.FirstOrDefault(File.Exists);
+            }
+            catch (Exception ex)
+            {
+                LoggingService.LogError($"Failed to find config path", ex);
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Searches for configuration file up through solution root directory
+        /// </summary>
+        private static IEnumerable<string> GetConfigPathBetweenPaths(string path, string root)
+        {
+            var configDirectory = File.GetAttributes(path).HasFlag(FileAttributes.Directory) ? path : Path.GetDirectoryName(path);
+
+            while (configDirectory.StartsWith(root, StringComparison.InvariantCultureIgnoreCase))
+            {
+                yield return Path.Combine(configDirectory, "Settings.XamlStyler");
+                configDirectory = Path.GetDirectoryName(configDirectory);
+            }
         }
     }
 }
