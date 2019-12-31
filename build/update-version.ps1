e2dc1b1aad694bb55edf35ebbe766948d8bb7ac2
Dave Grochocki <grochocki@users.noreply.github.com>
2019-12-30 20:53:59 -0800

    Update copyright and add versioning for nusepc and console proje… (#232)

--- build/update-version.ps1
+++ build/update-version.ps1
@@ -1,9 +1,23 @@
 $version = $args[0]
 Write-Host "Set version: $version"
 
-# TODO: Replace the path with the path to your vsixmanifest file
-$FullPath = Resolve-Path $PSScriptRoot\..\XamlStyler.Package\source.extension.vsixmanifest
-Write-Host $FullPath
-[xml]$content = Get-Content $FullPath
+# Update VSIX Version
+$vsixPath = Resolve-Path $PSScriptRoot\..\XamlStyler.Package\source.extension.vsixmanifest
+Write-Host $vsixPath
+$content = [xml] (Get-Content $vsixPath)
 $content.PackageManifest.Metadata.Identity.Version = $version
-$content.Save($FullPath)
+$content.Save($vsixPath)
+
+# Update Console Version
+$consolePath = Resolve-Path $PSScriptRoot\..\XamlStyler.Console\XamlStyler.Console.csproj
+Write-Host $consolePath
+$content = [xml] (Get-Content $consolePath)
+$content.Project.PropertyGroup.Version = $version
+$content.Save($consolePath)
+
+# Update Nuspec Version
+$nuspecPath = Resolve-Path $PSScriptRoot\..\build\nuget\XamlStyler.Console.nuspec
+Write-Host $nuspecPath
+$content = [xml] (Get-Content $nuspecPath)
+$content.Package.Metadata.Version = $version
+$content.Save($nuspecPath)
