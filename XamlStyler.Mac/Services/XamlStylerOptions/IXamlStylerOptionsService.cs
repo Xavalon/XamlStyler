a146c4c8bfaf5b98e0eec8b87f4d095dd411242a
Taras Shevchuk <schew4uk@gmail.com>
2020-01-11 02:02:35 +0200

    Visual Studio for Mac Updates (#246)
    * Partially fixed Mac support
    
    * Updated readme. Another extension checking
    
    * [WIP] Format document on save feature in progress
    
    * [WIP] Added context menu
    
    * [WIP] Newtonsoft downgrade
    
    * wip
    
    * [WIP] Mac extension refactoring
    
    * [WIP] Xaml styler Mac refactoring
    
    * [WIP] External paths fixes
    
    * Made hidden ResetToDefault option
    
    * Format Xaml on Save implementation
    
    * Finished all preparations
    
    * empty line
    
    * Update README.md
    
    * Reverted changes
    
    * Separate class for colors. Tabs fixes
    
    * Code review
    
    * Added copyright
    
    * Update README.md

---
+++ XamlStyler.Mac/Services/XamlStylerOptions/IXamlStylerOptionsService.cs
@@ -0,0 +1,21 @@
+﻿// © Xavalon. All rights reserved.
+
+using MonoDevelop.Ide.Gui;
+using MonoDevelop.Projects;
+using Xavalon.XamlStyler.Core.Options;
+
+namespace Xavalon.XamlStyler.Mac.Services.XamlStylerOptions
+{
+    public interface IXamlStylerOptionsService
+    {
+        IStylerOptions GetGlobalOptions();
+
+        void SaveGlobalOptions(IStylerOptions options);
+
+        void ResetGlobalOptions();
+
+        IStylerOptions GetDocumentOptions(Document document);
+
+        IStylerOptions GetDocumentOptions(string documentFilePath, Solution solution);
+    }
+}
