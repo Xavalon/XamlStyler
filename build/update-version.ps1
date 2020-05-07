0b1f81b7897279fb992f4a307e1c91c195ef678f
Dave Grochocki <grochocki@users.noreply.github.com>
2020-05-06 22:40:36 -0700

    build script

--- build/update-version.ps1
+++ build/update-version.ps1
@@ -2,14 +2,14 @@ $version = $args[0]
 Write-Host "Set version: $version"
 
 # Update VSIX Version
-$vsixPath = Resolve-Path $PSScriptRoot\..\src\XamlStyler.Extension.Windows\source.extension.vsixmanifest
+$vsixPath = Resolve-Path $PSScriptRoot\..\XamlStyler.Extension.Windows\source.extension.vsixmanifest
 Write-Host $vsixPath
 $content = [xml] (Get-Content $vsixPath)
 $content.PackageManifest.Metadata.Identity.Version = $version
 $content.Save($vsixPath)
 
 # Update Console Version
-$consolePath = Resolve-Path $PSScriptRoot\..\src\XamlStyler.Console\XamlStyler.Console.csproj
+$consolePath = Resolve-Path $PSScriptRoot\..\XamlStyler.Console\XamlStyler.Console.csproj
 Write-Host $consolePath
 $content = [xml] (Get-Content $consolePath)
 $content.Project.PropertyGroup.PackageVersion = $version
