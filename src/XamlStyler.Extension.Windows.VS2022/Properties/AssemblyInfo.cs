254bdd023aeca415392619d2fac82a1c1893cf47
Victor Irzak <victor.irzak@zomp.com>
2021-06-18 22:18:07 -0400

    Add VS2022 project

---
+++ src/XamlStyler.Extension.Windows.VS2022/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("XamlStyler.Extension.Windows")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Xavalon")]
+[assembly: AssemblyProduct("XamlStyler.Extension.Windows")]
+[assembly: AssemblyCopyright("Copyright © Xavalon 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(false)]
+//[assembly: NeutralResourcesLanguage("en-US")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en")]
